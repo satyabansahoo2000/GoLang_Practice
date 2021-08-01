8c4d3f8ad3b5e51ecc2c58bf7785a5d3142bfece
satyabansahoo2000 <satyaban20@gmail.com>
2021-08-01 21:35:59 +0530

    added Slices User-Defined Functions

--- slices/README.md
+++ slices/README.md
@@ -10,3 +10,11 @@ var_name := make([]data_type, size, capacity)
     - if we exceed the `cap` value, it will allocate more `cap` value
     - for example - we set it to 10, and we have input 8 elements at first, the `cap` value will be 10 but if we add 3 more elements, total elements will be 11 then the `cap` value will increase to 20.
 - `ptr`: A pointer that points to the address of the underlying array
+- Arrays won't change even if we create an array using original array
+- Assigning a slice to a new variable will create another slice that points to the same
+array pointed to by the original slice using the `copy()` function.
+```go
+copy(destination, source)
+```
+- the `copy()` function examines the length of both the destination and source slices and copies the minimum of these two numbers of elements
+- Slice doesn't have any insert or remove function, so we need to make an `insert()` function to insert an element in between and `remove()` to remove an element from any index.
--- slices/main.go
+++ slices/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
 
 func main() {
@@ -43,4 +44,40 @@ func main() {
 	for index, value := range slice2 {
 		fmt.Println("Index :", index, "Value: ", value)
 	}
+
+	// Copying a slice using copy()
+	slice3 := make([]int, len(slice2))
+	copy(slice3, slice2)
+	fmt.Println(slice3)
+	
+	// Insert an element in the slice
+	slice3, _ = insert(slice3, 4, 9)
+	fmt.Println(slice3)
+
+	// Remove an elements in the slice
+	slice3, _ = remove(slice3, 3)
+	fmt.Println(slice3)
+}
+
+// Function to insert an element in the slice
+func insert(slice []int, index, value int) ([]int, error) {
+	if index < 0 {
+		return nil, errors.New("index cannot be less than 0")
+	}
+	if index >= len(slice) {
+		return append(slice, value), nil
+	}
+
+	slice = append(slice[:index+1], slice[index:]...)			// variadic function
+	slice[index] = value
+	return slice, nil
+}
+
+// Function to delete an element from the slice
+func remove(slice []int, index int) ([]int, error) {
+	if index < 0 || index >= len(slice) {
+		return nil, errors.New("index out of the range")
+	}
+	slice = append(slice[:index], slice[index+1:]...)
+	return slice, nil
 }
